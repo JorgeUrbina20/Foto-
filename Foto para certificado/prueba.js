7919a54071d471823aeb4823f47018731a3adb3f
JorgeUrbina20 <157077203+JorgeUrbina20@users.noreply.github.com>
2025-01-19 11:54:33 -0600

    In this commit i have modified the document .js and added a document with the extension .sql for reinforcing that language.

--- Foto para certificado/prueba.js
+++ Foto para certificado/prueba.js
@@ -1,2 +1,4 @@
 let msj="Hola mundo";
 console.info(msj);
+
+//Esta es la linea de código sobre el cual se creará  la UI para la API
---
+++ Reinforcement of SQL.sql
@@ -0,0 +1,18 @@
+/*Este es un archivo de SQL Server para prácticar todos los conocimientos adquiridos
+en este lenguaje de programación como un lenguaje de consulta estructurado.*/
+
+--BDD
+--Tablas
+--Columnas
+--Filas
+
+-->Operadores (Aritméticos, Relación, Lógicos).
+-->Funciones (Matematicas,HoraFecha,String).
+
+--Vistas
+--Esquemas
+--Procedimientos almacenados
+-->Estructura de selección
+-->Transacciones
+-->Manejo de errores
+--Trigger
