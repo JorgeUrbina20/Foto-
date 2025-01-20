558a21dd9fba0033977c11fcaef95b52e60c526a
JorgeUrbina20 <157077203+JorgeUrbina20@users.noreply.github.com>
2025-01-19 18:34:37 -0600

    i have added some lines of code on that document.

--- Reinforcement of SQL.sql
+++ Reinforcement of SQL.sql
@@ -1,13 +1,57 @@
 /*Este es un archivo de SQL Server para prácticar todos los conocimientos adquiridos
 en este lenguaje de programación como un lenguaje de consulta estructurado.*/
 
---BDD
---Tablas
---Columnas
---Filas
+-->BDD
+create database pruebaF
+use PruebaFinal
+alter database pruebaF modify name="PruebaFinal";
+use master
+drop database "PruebaFinal"
+
+-->Tablas
+create table person(
+id int,
+nombre nvarchar(50) not null
+);
+
+execute sp_rename 'persona', 'person';
+
+drop table person
+
+insert into person  values(1,'Jose'),(2,'Pedro');
+
+select id,nombre from person;
+
+-->Columnas
+alter table person add age int;
+select * from person;
+
+alter table person drop column sex;
+
+exec sp_rename 'person.nombre','name';
+update person set age=22;
+
+-->Filas
+delete from person where id=2;
+truncate table person;
 
 -->Operadores (Aritméticos, Relación, Lógicos).
 -->Funciones (Matematicas,HoraFecha,String).
+-->Inner Join
+-->Group by / having
+-->operador like
+use prueba
+
+insert into productos values ('Agua',3),('Camisas',7),('Pailas',10);
+
+select * from productos
+
+select distinct Nombre from productos
+
+select count (precio) as cantidad, Nombre from productos group by Nombre
+having COUNT (precio)<5;
+
+select * from productos where Nombre like'a%';
 
 --Vistas
 --Esquemas
@@ -16,3 +60,8 @@ en este lenguaje de programación como un lenguaje de consulta estructurado.*/
 -->Transacciones
 -->Manejo de errores
 --Trigger
+
+--To practice all what i´ve seen.
+
+
+
