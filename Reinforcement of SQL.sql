9679535bcc5fb6d061203a7e0b14bc095b0e4c4c
JorgeUrbina20 <157077203+JorgeUrbina20@users.noreply.github.com>
2025-01-19 21:08:44 -0600

    Se ha empezado el proyecto en SQL Server donde se pondran en prueba todos los conocimientos obtenidos en este lenguaje de programacion como, lenguaje de consulta estructurado.

--- Reinforcement of SQL.sql
+++ Reinforcement of SQL.sql
@@ -62,6 +62,35 @@ select * from productos where Nombre like'a%';
 --Trigger
 
 --To practice all what i´ve seen.
+use pruebaFinal
+
+create table dep(
+id int primary key not null,
+nombreDep nvarchar(50) not null,
+states bit default 1 -->It means that the number one is "active" and the number zero is "innactive"
+);
+create table mun(
+id int primary key not null,
+nombreMun nvarchar(50) not null,
+idDep int foreign key references dep(id)
+);
+create table neigh(
+id int primary key not null,
+nombreNei nvarchar(50) not null,
+idMun int foreign key references mun(id)
+);--This is the neighbordhood
+
+create table employee (
+id int primary key,
+fullName nvarchar(130),
+sex char(1),
+dni char(16) check (dni like ('[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9][A-Z]')),
+salary float not null
+);
+
+create table 
+--Repasar los constrainst 
+
 
 
 
